d23a0ad02de6165c5c7397303b11dcb3caad4a69
deequoique <puppy_road@163.com>
2022-10-06 13:47:11 +0800

    card优化

---
+++ app/src/main/java/com/example/teamupboard/adapter/CardMasterTypeAdapter.java
@@ -0,0 +1,29 @@
+package com.example.teamupboard.adapter;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CardMasterTypeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private Context context;
+    private String data;
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
